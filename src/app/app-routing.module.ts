8961ea0e518a5e7b9d58c501aa3d8bb8e19b6adb
jimmy cruz <cruzperezjimmya@gmail.com>
2023-01-12 12:08:17 -0500

    se añade navbar y navegacion por rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // importando rutas
+  {path:'', component:HomeComponent},
+  {path:'nosotros', component:NosotrosComponent},
+  {path:'contacto', component:ContactoComponent},
+  {path:'**', pathMatch:'full', redirectTo:''}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
